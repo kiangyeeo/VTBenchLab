77d1d4ecdd231c4ac583124c7a2ff02bf716e4c5
kiangyeeo <wkengyi123@gmail.com>
2026-06-16 05:13:15 +0000

    fix openai pretrain config

--- CLIP_benchmark/clip_benchmark/models/open_clip.py
+++ CLIP_benchmark/clip_benchmark/models/open_clip.py
@@ -2,7 +2,14 @@ import open_clip
 
 
 def load_open_clip(model_name: str = "ViT-B-32-quickgelu", pretrained: str = "laion400m_e32", cache_dir: str = None, device="cpu"):
-    model, _, transform = open_clip.create_model_and_transforms(model_name, pretrained=pretrained, cache_dir=cache_dir)
+    pretrained_cfg = open_clip.get_pretrained_cfg(model_name, pretrained)
+    force_quick_gelu = bool(pretrained_cfg and pretrained_cfg.get("quick_gelu"))
+    model, _, transform = open_clip.create_model_and_transforms(
+        model_name,
+        pretrained=pretrained,
+        cache_dir=cache_dir,
+        force_quick_gelu=force_quick_gelu,
+    )
     model = model.to(device)
     tokenizer = open_clip.get_tokenizer(model_name)
     return model, transform, tokenizer
