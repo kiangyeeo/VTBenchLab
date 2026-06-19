2b399978401bea64b472079a778f9afed20cfcbb
kiangyeeo <wkengyi123@gmail.com>
2026-06-19 10:57:33 +0000

    fix tokenflow

--- TokBench/tokenzier_vae_scripts/image_scripts/tokenflow_rec.py
+++ TokBench/tokenzier_vae_scripts/image_scripts/tokenflow_rec.py
@@ -60,7 +60,6 @@ def load_tokenflow(args, device):
         teacher="clipb_224",
         enhanced_decoder=True,
         infer_interpolate=True,
-        resolution=args.padding_size,
     )
     vq_model.to(device)
     vq_model.eval()
